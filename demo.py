480011e71cda963a4306047ea03da2a483daa65b
s1290173 <109272010+s1290173@users.noreply.github.com>
2025-07-11 11:28:57 +0900

    Update demo.py

--- demo.py
+++ demo.py
@@ -3,7 +3,7 @@ import time
 if __name__=="__main__":
     g=MyGripper_H100("/dev/ttyCH343USB0",baudrate=115200,id=14)##填写实际的串口号和波特率和夹爪ID
     print("夹爪的实际ID为:",g.get_gripper_Id())
-    print(g.set_gripper_joint_angle(0,50))
+    print(g.set_gripper_joint_angle(1,50))
     time.sleep(2)
     print(g.set_gripper_joint_angle(0,))
     time.sleep(2)
