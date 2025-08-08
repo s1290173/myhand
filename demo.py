c51e8631f5e4085dfb9bf0d8f8b4c8396be0b348
s1290173 <109272010+s1290173@users.noreply.github.com>
2025-08-08 19:58:57 +0900

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,31 +1,9 @@
-from MyHand import MyGripper_H100
+from elegripper import Gripper
 import time
 if __name__=="__main__":
-    g=MyGripper_H100("/dev/ttyCH343USB0",baudrate=115200,id=14)##填写实际的串口号和波特率和夹爪ID
+    g=Gripper("COM27",baudrate=115200,id=14)##填写实际的串口号和波特率和夹爪ID
     print("夹爪的实际ID为:",g.get_gripper_Id())
-    print(g.set_gripper_joint_angle(1,50))
+    print(g.set_gripper_value(100,100))
     time.sleep(2)
-    print(g.set_gripper_joint_angle(1,0))
+    print(g.set_gripper_value(0,100))
     time.sleep(2)
-    print(g.set_gripper_joint_angle(2,100))
-    time.sleep(2)
-    print(g.set_gripper_joint_angle(2,0))
-    time.sleep(2)
-    print(g.set_gripper_joint_angle(3,50))
-    time.sleep(2)
-    print(g.set_gripper_joint_angle(3,0))
-    time.sleep(2)
-    print(g.set_gripper_joint_angle(4,50))
-    time.sleep(2)
-    print(g.set_gripper_joint_angle(4,0))
-    time.sleep(2)
-    print(g.set_gripper_joint_angle(5,50))
-    time.sleep(2)
-    print(g.set_gripper_joint_angle(5,0))
-    time.sleep(2)
-    print(g.set_gripper_joint_angle(6,50))
-    time.sleep(2)
-    print(g.set_gripper_joint_angle(6,0))
-    time.sleep(2)
-
-
