4ec1d0951f28466869a5ea09662662cc71421bed
s1290173 <109272010+s1290173@users.noreply.github.com>
2025-08-11 16:04:49 +0900

    Update demo.py

--- demo.py
+++ demo.py
@@ -3,7 +3,7 @@ import time
 if __name__=="__main__":
     g=Gripper("/dev/ttyAMA0",baudrate=115200,id=14)##填写实际的串口号和波特率和夹爪ID
     print("夹爪的实际ID为:",g.get_gripper_Id())
-    print(g.set_gripper_value(100,100))
+    print(g.set_gripper_value(0,0))
     time.sleep(2)
     print(g.set_gripper_value(0,100))
     time.sleep(2)
